5bba9fa57c7c41165da3c249f0368955b7a6ccd3
Martin-J-Larre <martinjlarre@gmail.com>
2023-12-29 23:33:25 -0500

    #4 add supabase for handle api and db

---
+++ src/services/supabaseExample.js
@@ -0,0 +1,10 @@
+/*
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://mnguvbgli.supabase.co";
+const supabaseKey = "Api_key"
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+
+*/
