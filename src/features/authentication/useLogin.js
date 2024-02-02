6299165b71ad1e3c3d309551cdd89d47af3b1af8
Martin-J-Larre <martinjlarre@gmail.com>
2024-02-01 22:27:09 -0500

    #28 Login with react-hook-form

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -4,12 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 export function useLogin() {
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
   const { isLoading, mutate: login } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      // queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
       console.log(user);
     },
